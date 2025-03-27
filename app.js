f6950b81e1c5d2e84ba5909fc5d1443a0d20628b
jf-pp <jfpootpool@gmail.com>
2025-03-27 04:41:25 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+const INPUT_AMIGO = document.getElementById("amigo");
+const UL_LISTA_AMIGOS = document.getElementById("listaAmigos");
+const UL_RESULTADO = document.getElementById("resultado");
+var listaAmigos = [];
+
+function agregarAmigo(){
+    if(!INPUT_AMIGO.value){
+        alert("Debes ingresar un nombre");
+        return;
+    }
+
+    listaAmigos.push(INPUT_AMIGO.value);
+    UL_LISTA_AMIGOS.innerHTML += `<li>${INPUT_AMIGO.value}</li>`;
+    //console.log(UL_LISTA_AMIGOS)
+};
+
+function sortearAmigo(){
+    if(listaAmigos.length === 0){
+        alert("Debes ingresar un nombre");
+        return;
+    }
+
+    var cantidadAmigos = listaAmigos.length;
+    const random = Math.floor(Math.random() * cantidadAmigos);
+    const amigoSecreto = listaAmigos[random];
+    UL_RESULTADO.innerHTML = `<li>${amigoSecreto}</li>`;
+};
